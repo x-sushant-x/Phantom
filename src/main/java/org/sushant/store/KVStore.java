4f72fd2095c4f8936ea407ba878122943a082fd0
Sushant <sushant.dhiman9812@gmail.com>
2025-07-29 09:23:55 +0530

    wrote test cases for KV Store

--- src/main/java/org/sushant/store/KVStore.java
+++ src/main/java/org/sushant/store/KVStore.java
@@ -1,11 +1,18 @@
 package org.sushant.store;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Data
 public class KVStore {
     private ConcurrentHashMap<String, Object> store;
+
+    KVStore() {
+        this.store = new ConcurrentHashMap<>();
+    }
 
     public void set(String key, String value) {
         store.put(key, value);
@@ -14,10 +21,15 @@ public class KVStore {
     public <T> T get(String key, Class<T> clazz) {
         Object value = store.get(key);
 
+        if(value == null) {
+            return null;
+        }
+
         if(clazz.isInstance(value)) {
             return clazz.cast(value);
         } else {
-            throw new ClassCastException("Cannot cast " + value + " to " + clazz.getName());
+            String msg = String.format("Cannot cast %s of type %s to %s", value, value.getClass(), clazz);
+            throw new ClassCastException(msg);
         }
     }
 }
