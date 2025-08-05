f518c75052590b61ecd68b156eaa376f69aa6a1e
Sushant <sushant.dhiman9812@gmail.com>
2025-08-05 22:24:48 +0530

    added a ConfigLoader class to load data from application.properties

---
+++ src/main/java/org/sushant/utils/ConfigLoader.java
@@ -0,0 +1,29 @@
+package org.sushant.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigLoader {
+    private static final Properties properties = new Properties();
+
+    static {
+        try(InputStream input = ConfigLoader.class.getClassLoader().getResourceAsStream("application.properties")) {
+            if (input == null) {
+                throw new RuntimeException("application.properties not found in classpath");
+            }
+
+            properties.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load application.properties", e);
+        }
+    }
+
+    public static String get(String key) {
+        return properties.getProperty(key);
+    }
+
+    public static int getInt(String key) {
+        return Integer.parseInt(get(key));
+    }
+}
